4f4aa06eaf69fd5881a353d3d0b3c248f3f5b438
Chandan KL <chandan.kl@project44.com>
2025-07-26 14:43:44 +0530

    Update next config to support all image hostnames

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@mastra/*"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
 };
 
 if (process.env.EXPORT) {
